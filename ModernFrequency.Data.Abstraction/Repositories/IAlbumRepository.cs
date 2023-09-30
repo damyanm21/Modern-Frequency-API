f892eebd2bdbe169be812b1a2e460e8344be10e9
damyan.dobromirov <damyan.dobromirov@university.mentormate.com>
2023-09-30 12:14:25 +0300

    added repositories concrete classes and repositories interfaces documentation

--- ModernFrequency.Data.Abstraction/Repositories/IAlbumRepository.cs
+++ ModernFrequency.Data.Abstraction/Repositories/IAlbumRepository.cs
@@ -2,6 +2,10 @@
 
 namespace ModernFrequency.Data.Abstraction.Repositories
 {
+    /// <summary>
+    /// Represents a repository interface for managing Album entities.
+    /// This interface inherits IRepository<Album>
+    /// </summary>
     public interface IAlbumRepository : IRepository<Album>
     {
     }
---
+++ ModernFrequency.Data/Repositories/TrackRepository.cs
@@ -0,0 +1,12 @@
+﻿using ModernFrequency.Data.Abstraction.Repositories;
+using ModernFrequency.Data.Models.Models;
+
+namespace ModernFrequency.Data.Repositories
+{
+    public class TrackRepository : Repository<Track>, ITrackRepository
+    {
+        public TrackRepository(ModernFrequencyDbContext context) : base(context)
+        {
+        }
+    }
+}
