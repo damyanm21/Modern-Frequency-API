e36195541308c2b08ac90a89130a429f47956a84
damyan.dobromirov <damyan.dobromirov@university.mentormate.com>
2023-09-30 15:03:22 +0300

    added abstract services documentation

--- ModernFrequency.Business.Abstraction/Services/IArtistService.cs
+++ ModernFrequency.Business.Abstraction/Services/IArtistService.cs
@@ -2,12 +2,40 @@
 
 namespace ModernFrequency.Business.Abstraction.Services
 {
+    /// <summary>
+    /// Represents a service for managing artists.
+    /// </summary>
     public interface IArtistService
     {
+        /// <summary>
+        /// Gets all artists asynchronously.
+        /// </summary>
+        /// <returns>An enumerable collection of artist DTOs.</returns>
         Task<IEnumerable<ArtistDTO>> GetAllArtistsAsync();
+
+        /// <summary>
+        /// Gets an artist by their ID asynchronously.
+        /// </summary>
+        /// <param name="id">The unique ID of the artist.</param>
+        /// <returns>The artist DTO if found; otherwise, null.</returns>
         Task<ArtistDTO> GetArtistByIdAsync(int id);
+
+        /// <summary>
+        /// Creates a new artist asynchronously.
+        /// </summary>
+        /// <param name="artist">The artist DTO to create.</param>
         Task CreateArtistAsync(ArtistDTO artist);
+
+        /// <summary>
+        /// Updates an existing artist asynchronously.
+        /// </summary>
+        /// <param name="artist">The artist DTO to update.</param>
         Task UpdateArtistAsync(ArtistDTO artist);
+
+        /// <summary>
+        /// Deletes an artist by their ID asynchronously.
+        /// </summary>
+        /// <param name="id">The unique ID of the artist to delete.</param>
         Task DeleteArtistAsync(int id);
     }
 }
